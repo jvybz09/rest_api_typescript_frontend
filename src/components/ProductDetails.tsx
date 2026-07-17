57302f727cdc0f862e72f3c148e8c8b8b5433b87
Js09 <jvybz09@gmail.com>
2026-07-16 18:37:52 -0600

    first commit

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,74 @@
+import { useNavigate, Form, type ActionFunctionArgs, redirect, useFetcher } from 'react-router-dom'
+import type { Product } from "../types";
+import { formatCurrency } from "../utils";
+import { deleteProduct } from '../services/ProductService';
+
+type ProductDetailsProps = {
+    product : Product
+}
+
+//Creamos la funcion
+export async function action({params} : ActionFunctionArgs){
+    if(params.id !== undefined){
+        await deleteProduct(+params.id)
+        
+        return redirect('/')
+    }
+
+}
+
+export default function ProductDetails({product} : ProductDetailsProps) {
+
+    const fetcher = useFetcher()
+    const navigate = useNavigate()
+    const isAvailable = product.availability
+
+  return (
+    <tr className="border-b ">
+        <td className="p-3 text-lg text-gray-800">
+            {product.name}
+        </td>
+        <td className="p-3 text-lg text-gray-800">
+            {formatCurrency(product.price)}
+        </td>
+        <td className="p-3 text-lg text-gray-800">
+            <fetcher.Form method="POST">
+                <button
+                    type="submit"
+                    name="id"
+                    value={product.id}
+                    className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-black w-full shadow hover:shadow-lg transition duration-200 text-center cursor-pointer`}
+                >
+                    {isAvailable ? 'Disponible' : 'No Disponible'}
+                </button>
+            </fetcher.Form>
+        </td>
+        <td className="p-3 text-lg text-gray-800 ">
+           <div className="flex gap-2 items-center">
+            <button 
+                onClick={() => navigate(`/productos/${product.id}/editar`)}
+                className="cursor-pointer bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+            >Editar</button>
+
+            <Form
+                className="w-full"
+                method="POST"
+                action={`productos/${product.id}/eliminar`}
+                onSubmit={(e) => {
+                    if(!confirm('Deseas eliminar?')){
+                        e.preventDefault()
+                    }
+                }}
+            >
+                <input
+                    className="cursor-pointer bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs"
+                    type="submit"
+                    value="Eliminar"
+                />
+            </Form>
+           </div>
+        </td>
+    </tr> 
+  )
+}
+
---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <>
+        <header className="bg-slate-800">
+            <div className="max-w-6xl mx-auto py-10">
+                <h1 className="text-4xl font-extrabold text-white">
+                    Administrador de Productos
+                </h1>
+            </div>
+        </header>
+        
+        <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow rounded-lg">
+          <Outlet/>
+        </main>
+    </>
+  )
+}
+
